ae3222d01513981982b70b5826339135e62d42ee
Daniel Gonzalez <Alphagame1988@outlook.de>
2020-10-25 23:16:47 +0100

    HF

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+    <div id='HeadPic'>
+        <img src='../assets/IchRund.png' style='max-width: 5rem'>
+    </div>
+    <div id='ProfilName'>
+            <h1> Daniel Gonzalez </h1>
+    </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    width: 100%;
+    height: 10%;
+    padding: 1rem 0;
+    background-color: black;
+    top: 0%;
+    position: fixed;
+    #HeadPic {
+        margin: 0px 0px 0px 15px;
+        padding: 0px;
+        width: 30%;
+        float: left;
+    }
+    #ProfilName {
+        width: 30%;
+        margin: 0px 0px 0px 0px;
+        padding: 0px;
+        color: white;
+        text-align: center;
+        float: left;
+    }
+}
+</style>
